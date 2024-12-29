e8af40ff646b96cdd818fbb9c815366b599f428e
gemechis-elias <gemcs.elias@gmail.com>
2024-12-29 12:32:50 +0300

    Upgaraded Flutter Version

--- android/app/src/main/kotlin/com/example/myexpensetracker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myexpensetracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/out_of_pocket/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.outofpocket.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
